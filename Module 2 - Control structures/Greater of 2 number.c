1358ece8d2a3cfff844cf3413cbe75c5b9f89e73
Pushkar-Adari <101546155+Pushkar-Adari@users.noreply.github.com>
2022-03-14 21:06:59 +0530

    Create Greater of 2 number.c

---
+++ Module 2 - Control structures/Greater of 2 number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    int a,b;
+    printf ("Enter the first number: ");
+    scanf ("%d",&a);
+    printf ("Enter the second number: ");
+    scanf ("%d",&b);
+    
+    if (a>b)
+    {
+        printf ("A is bigger");
+    }
+    else
+    {
+        printf ("B is bigger");
+    }
+}
